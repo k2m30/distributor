771c1b0b499a4bfaaf181ead594ac4bb6e96eab3
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2013-09-25 19:37:16 +0300

    deploy

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Distibutor
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
     config.assets.initialize_on_precompile = false
+
+    config.assets.enabled = true
   end
 end
