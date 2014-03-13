6bfd1e497301ffb9858a597ea6276d6ea2b0f477
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-03-14 01:10:26 +0300

    -

--- app/models/site.rb
+++ app/models/site.rb
@@ -208,7 +208,7 @@ class Site < ActiveRecord::Base
 
   def parsing_site_method1
     begin
-      logger.warn "------ start parsing function method1 " + self.name + "------"
+      logger.warn "------ start parsing function method1 " + self.name + "------" + ENV['RACK_ENV']
       if ENV['RACK_ENV'] == 'production'
         logger.error 'Headless started'
         headless = Headless.new
